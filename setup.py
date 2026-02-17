c60ad404561c3c28c70bcd0c7718c8d3205ce9bd
rgs2151 <rgs2151@columbia.edu>
2026-02-16 19:58:54 -0600

    package change

--- setup.py
+++ setup.py
@@ -1,5 +1,2 @@
-from setuptools import setup, find_packages
-
-# NOTE: All project metadata and dependencies are defined in pyproject.toml.
-# This file exists only for legacy editable-install compatibility.
+from setuptools import setup
 setup()
